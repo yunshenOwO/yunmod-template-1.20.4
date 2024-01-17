f2d5ec02c2b3bdb21fcff5c9504aa02b19c91dd7
云深不知处 <luoguixin1234@gmail.com>
2024-01-17 18:09:00 +0800

    删除盔甲

--- src/main/kotlin/com/yunshen/item/ItemRegistry.kt
+++ src/main/kotlin/com/yunshen/item/ItemRegistry.kt
@@ -1,11 +1,8 @@
 package com.yunshen.item
 
 import com.yunshen.YunMod.logger
-import com.yunshen.block.CustomArmorMaterial
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings
 import net.fabricmc.fabric.api.registry.FuelRegistry
-import net.minecraft.item.ArmorItem
-import net.minecraft.item.ArmorMaterial
 import net.minecraft.item.Item
 import net.minecraft.registry.Registries
 import net.minecraft.registry.Registry
@@ -18,19 +15,8 @@ object ItemRegistry {
         Registries.ITEM, Identifier(MODID, "custom_item"), ModItem(
         FabricItemSettings()
     ))
-    private val CUSTOM_ARMOR_MATERIAL: ArmorMaterial = CustomArmorMaterial()
-    private val CUSTOM_MATERIAL = ModItem(Item.Settings())
-    private val CUSTOM_MATERIAL_HELMET: Item = ArmorItem(CUSTOM_ARMOR_MATERIAL, ArmorItem.Type.HELMET, Item.Settings())
-    private val CUSTOM_MATERIAL_CHESTPLATE: Item = ArmorItem(CUSTOM_ARMOR_MATERIAL, ArmorItem.Type.BOOTS, Item.Settings())
-    private val CUSTOM_MATERIAL_LEGGINGS: Item = ArmorItem(CUSTOM_ARMOR_MATERIAL, ArmorItem.Type.LEGGINGS, Item.Settings())
-    private val CUSTOM_MATERIAL_BOOTS: Item = ArmorItem(CUSTOM_ARMOR_MATERIAL, ArmorItem.Type.CHESTPLATE, Item.Settings())
     fun registriesItemAll(){
         Registry.register(Registries.ITEM_GROUP, Identifier(MODID, "test_group"), ModItemGroups.ITEM_GROUP) //注册物品栏
-        Registry.register(Registries.ITEM, Identifier(MODID, "custom_material"), CUSTOM_MATERIAL)
-        Registry.register(Registries.ITEM, Identifier(MODID, "custom_material_helmet"), CUSTOM_MATERIAL_HELMET)
-        Registry.register(Registries.ITEM, Identifier(MODID, "custom_material_chestplate"), CUSTOM_MATERIAL_CHESTPLATE)
-        Registry.register(Registries.ITEM, Identifier(MODID, "custom_material_leggings"), CUSTOM_MATERIAL_LEGGINGS)
-        Registry.register(Registries.ITEM, Identifier(MODID, "custom_material_boots"), CUSTOM_MATERIAL_BOOTS)
         logger.info("物品注册完成")
     }
     fun addOtherEffect(){
